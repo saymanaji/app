5ff56bef353e5d8502bce7da10217d2a2090c872
saymanaji <seimanaji851@gmail.com>
2023-03-12 18:15:50 +0100

    app

---
+++ components/Regform.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet,
+     Text,
+      View,
+    TouchableOpacity, } from 'react-native';
+
+export default class Regform extends Component {
+    render(){
+        return (
+            <View style={styles.regform} >
+                <Text>Testing</Text>
+            </View>
+        );
+    }
+  
+}
+
+const styles = StyleSheet.create({
+
+});
